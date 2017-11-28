563f3d6702fed15582f2e68cc5ee96bc287c40b4
Rob Lim <robert.m.lim@gmail.com>
2017-11-27 20:35:13 -0800

    Create sample data file

--- entry.jsx
+++ entry.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Cashflow from './cashflow';
+import { SINGLE, JOINT } from './data';
+
+const userBirthday = "1987-09-18";
+const spouseBirthday = "1980-02-17";
 
 document.addEventListener("DOMContentLoaded", () => {
 	const root = document.getElementById("root");
-	ReactDOM.render(<Cashflow/>, root);
+	ReactDOM.render(<Cashflow
+										cashflow={ SINGLE }
+										joint={ false }
+										userBirthday={ userBirthday }
+										spouseBirthday={ spouseBirthday }
+										/>, root);
 });
