48870f0d6b2f7d296957291804bb6b1c003c7b0c
Rob Lim <robert.m.lim@gmail.com>
2017-11-27 20:17:44 -0800

    Initial commit

---
+++ cashflow.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Cashflow = () => <h1>Congratulations, you did it!</h1>;
+
+export default Cashflow;
---
+++ entry.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Cashflow from './cashflow';
+
+document.addEventListener("DOMContentLoaded", () => {
+	const root = document.getElementById("root");
+	ReactDOM.render(<Cashflow/>, root);
+});
